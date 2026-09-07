a79c06c7077494377fa168cc7b654d23f0205f36
Kimonaro <g.kira@escolar.ifrn.edu.br>
2026-09-07 20:06:56 -0300

    bandeira branca
    arreguei, tirei o PUN e tentei fazer outra coisa, mas é muito erro que eu não sei como resolver

--- Robô/Assets/Scripts/OnlineManager.cs
+++ Robô/Assets/Scripts/OnlineManager.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
-using Photon.pun;
-using Photo.Realtime;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
 
-public class OnlineManager : MonoBehaviour
+public class OnlineManager : MonoBehaviourPunCallbacks
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        Debug.Log("conectado");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+       // base.OnDisconnected(cause);
+       Debug.Log("desconectado");
+    }
+
 }
