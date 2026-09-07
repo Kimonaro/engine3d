fc8e41671b98649da592f8c680add6ca58360bed
Kimonaro <g.kira@escolar.ifrn.edu.br>
2026-09-07 16:37:05 -0300

    Multi
    Tentativa Nº1 de multiplayer

---
+++ Robô/Assets/Scripts/OnlineManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Photon.pun;
+using Photo.Realtime;
+
+public class OnlineManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
